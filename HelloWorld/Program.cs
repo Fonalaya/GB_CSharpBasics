e051cc20a3de1e9be1f6cece51747e05be050b8c
Admin <Admin@NOTEBOOKPC01>
2023-11-25 19:16:17 +0300

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, как тебя зовут");
+            string name = Console.ReadLine();
+            string date = DateTime.UtcNow.ToString("d");
+            Console.WriteLine("Привет, " + name + ", сегодня " + date + ".");
+            Console.ReadLine();
+        }
+    }
+}
